4eb9e1dee5bc154b5be65cacee682851af63f0ac
yingsunnn <sco.suen@gmail.com>
2024-01-12 00:39:57 -0500

    Added navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,33 @@ interface PageProps {
   };
 }
 
+function getTitle({ q, type, location, remote }: JobFilterValues) {
+  const titlePrefix = q
+    ? `${q} jobs`
+    : type
+      ? `${type} jobs`
+      : remote
+        ? "Remote jobs"
+        : "All jobs";
+
+  const titleSuffic = location ? ` in ${location}` : "";
+
+  return `${titlePrefix}${titleSuffic}`;
+}
+
+export function generateMetadata({
+  searchParams: { q, type, location, remote },
+}: PageProps) {
+  return {
+    title: `${getTitle({
+      q,
+      type,
+      location,
+      remote: remote === "true",
+    })} | Job Board`,
+  };
+}
+
 export default async function Home({
   searchParams: { q, type, location, remote },
 }: PageProps) {
@@ -25,12 +52,12 @@ export default async function Home({
   return (
     <main className="m-auto my-10 max-w-5xl space-y-10 px-3">
       <div className="space-y-5 text-center">
-        <H1>Developer jobs </H1>
+        <H1>{getTitle(filterValues)}</H1>
         <p className="text-muted-foreground">Find your dream job.</p>
       </div>
       <section className="flex flex-col gap-4 md:flex-row">
-        <JobFilterSidebar defaultValues={filterValues}/>
-        <JobResults filterValues={filterValues}/>
+        <JobFilterSidebar defaultValues={filterValues} />
+        <JobResults filterValues={filterValues} />
       </section>
     </main>
   );
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import logo from "@/assets/logo.png";
+import Image from "next/image";
+import { Button } from "./ui/button";
+
+export default function Navbar() {
+  return (
+    <header className="shadow-sm">
+      <nav className="m-auto flex max-w-5xl items-center justify-between px-3 py-5">
+        <Link href="/" className="flex items-center gap-3">
+          <Image src={logo} width={40} height={40} alt="Logo"/>
+          <span className="text-xl font-bold tracking-tighter">Job Board</span>
+        </Link>
+        <Button asChild>
+          <Link href="/jobs/new">Post a job</Link>
+        </Button>
+      </nav>
+    </header>
+  );
+}
